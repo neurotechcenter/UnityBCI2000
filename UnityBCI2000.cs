d6bc5668627e50fb08e9522e8fe10aa7e685ba3d
Ty Butler <tytbu@tytbutler.com>
2024-05-10 16:09:52 -0500

    written

--- UnityBCI2000.cs
+++ UnityBCI2000.cs
@@ -3,10 +3,23 @@ using BCI2000RemoteNET;
 public class UnityBCI2000 : MonoBehaviour {
 
     ///<summary>
-    ///BCI2000Remote instance which handles control of BCI2000.
+    ///The connection to BCI2000. Control BCI2000 by accessing this member to run BCI2000Remote commands directly. For example, to set an event:
+    ///<code>
+    ///UnityBCI2000 bci = GameObject.Find(...).GetComponent<UnityBCI2000>();
+    ///bci.Control.SetEvent("event", 7);
+    ///</code>
+    ///This property cannot be accessed during scene initialization. In order to run BCI2000 commands during scene startup, use either <c>OnIdle</c> to run BCI2000 commands before its modules have started (for adding events, parameters, etc.), or <c>OnConnected</c> to run commands after modules have initialized. (If StartModules is set to false then OnIdle and OnConnected will be equivalent, they will both run during the <c>Start()</c> method.)
     ///</summary>
-    public BCI2000Remote control;
-
+    public BCI2000Remote Control {
+	private set;
+	get {
+	    if (!initialized) {
+		throw new Exception("Cannot directly run BCI2000 commands during scene initialization. If you wish to run a BCI2000 command, use OnIdle and OnConnected to run commands before or after BCI2000's modules have started");
+	    } else {
+		return control;
+	    }
+	}
+    }
     ///<summary>
     ///Start a local operator module listening on the address specified by OperatorAddress and OperatorPort
     ///</summary>
@@ -34,13 +47,21 @@ public class UnityBCI2000 : MonoBehaviour {
     public int ConnectTimeout = 1000; 
 
     ///<summary>
-    ///Start collecting data when the scene starts. If you want to start BCI2000 manually instead, call <c>control.Start()</c> 
-    ///If you are starting a BCI2000 run manually rather than setting StartWithScene (via <c>control.Start()</c>), avoid starting the run during scene initialization (i.e. in <c>Awake()</c> or <c>Start()</c> within a script), as this may cause the <see cref="OnConnect"/> commands to fail, depending on the order in which each <c>GameObject</c> initializes.
+    ///Start modules automatically when the scene loads. Set to false if you want to start modules manually, and call either <c>Control.StartupModules()</c> with the required arguments, or this object's <c>StartupModules()</c> method, which starts up modules according to the parameters set in the Unity inspector.
+    ///Note on manual control of BCI2000 initialization:
+    ///If you wish to control BCI2000 initialization manually, such as if your scene starts with a button press rather than automatically, <c>OnIdle</c> and <c>OnConnected</c> will likely not function as they do when starting BCI2000 automatically. In this case it would be more appropriate to control BCI2000 directly via <c>Control</c>.
+    ///</summary>
+    public bool StartModules = true;
+
+    ///<summary>
+    ///Start collecting data when the scene starts. If you want to start BCI2000 manually instead, call <c>Control.Start()</c> 
+    ///<see cref="StartModules"/> for a note on controlling BCI2000 manually.
     ///</summary>
     public bool StartWithScene = true;
 
     ///<summary>
-    ///Stop collecting data when scene stops. If you want to stop BCI2000 manually instead, call <c>control.Stop()</c>
+    ///Stop collecting data when scene stops. If you want to stop BCI2000 manually instead, call <c>Control.Stop()</c>
+    ///<see cref="StartModules"/> for a note on controlling BCI2000 manually.
     ///</summary>
     public bool StopWithScene = true;
 
@@ -52,22 +73,41 @@ public class UnityBCI2000 : MonoBehaviour {
     ///<summary>
     ///List of paths to parameter files to load
     ///</summary>
-    public List<string> parameterFiles;
+    public List<string> ParameterFiles;
 
     ///<summary>
-    ///Add an action to run immediately after connecting to BCI2000, before modules have started. This is useful for adding states, events, and parameters. This must be called within <c>Awake()</c> or it will have no effect. 
+    ///Add an action to run immediately after connecting to BCI2000, before modules have started. This is useful for adding states, events, and parameters. This must be called within <c>Awake()</c>.
     ///For example, to add an event:
     ///<code>
     ///UnityBCI2000 bci = ...
     ///bci.OnEvent(remote => remote.AddEvent("event"));
     ///</code>
     ///<paramref name="action"/> is an <c>Action<BCI2000Connection></c> which will be called with this object's BCI2000Connection. This can take the form of a lambda which takes one parameter, that being the <c>BCI2000Remote</c> object. 
-    ///If you are starting a BCI2000 run manually rather than setting StartWithScene (via <c>control.Start()</c>), avoid starting the run during scene initialization (i.e. in <c>Awake()</c> or <c>Start()</c> within a script), as this may cause the OnConnect commands to fail, depending on the order in which each <c>GameObject</c> initializes.
+    ///If you are starting a BCI2000 run manually rather than setting StartWithScene (via <c>Control.Start()</c>), avoid starting the run during scene initialization (i.e. in <c>Awake()</c> or <c>Start()</c> within a script), as this may cause the OnIdle commands to fail, depending on the order in which each <c>GameObject</c> initializes.
     ///</summary>
     ///<param name="action">The action which will be taken immediately after connecting to BCI2000, while it is in the Idle state. It takes one parameter, which will be this object's <c>BCI2000Remote</c> instance.
-    public void OnConnect(Action<BCI2000Remote> action) {
-	onConnect.Add(action);
+    ///<seealso cref="OnConnected"/>
+    public void OnIdle(Action<BCI2000Remote> action) {
+	if (awake_finished) {
+	    throw new Exception("Attempted to call OnIdle() outside of Awake(). OnIdle() must be called inside Awake()");
+	}
+	onIdle.Add(action);
     }
+
+    ///<summary>
+    ///Similar to <c>OnIdle</c>, but runs after BCI2000 modules have started. Can be used for setting parameters defined by modules. If <c>StartModules</c> is set to false, this is equivalent to <c>OnIdle</c>
+    ///</summary>
+    ///<param name="action"><c>Action<BCI2000Remote></c> which will be invoked immediately after modules are started within the <c>Start()</c> method.
+    public void OnIdle(Action<BCI2000Remote> action) {
+	if (awake_finished) {
+	    throw new Exception("Attempted to call OnConnected() outside of Awake(). OnIdle() must be called inside Awake()");
+	}
+	onConnected.Add(action);
+    }
+
+
+
+
 
     public string Module1 = "SignalGenerator";
     public string[] Module1Args;
@@ -78,18 +118,61 @@ public class UnityBCI2000 : MonoBehaviour {
     public string Module3 = "DummyApplication";
     public string[] Module3Args;
 
-    private List<Action<BCI2000Remote>> onConnect = new List<Action<BCI2000Remote>>();
+    private List<Action<BCI2000Remote>> onIdle = new List<Action<BCI2000Remote>>();
+    private List<Action<BCI2000Remote>> onConnected = new List<Action<BCI2000Remote>>();
+    
+    private bool initialized = false;
+    private bool awake_finished = false;
 
-    private BCI2000Connection = new BCI2000Connection();
-    private BCI2000Remote = new BCI2000Remote();
 
+    private BCI2000Remote control;
+
+
+    public void StartupModules() {
+	control.StartupModules(new Dictionary<string, List<string>>()
+	{
+	    {Module1, module1ArgsList },
+	    {Module2, module2ArgsList },
+	    {Module3, module3ArgsList }
+	});
+    }
 
     void Awake(){
-	control = new BCI2000Remote(new BCI2000Connection));
+	Control = new BCI2000Remote(new BCI2000Connection));
 	if (StartLocalOperator) {
 	    control.connection.StartOperator(OperatorPath, OperatorAddress, OperatorPort);
 	}
-	control.Connect(OperatorAddress, OperatorPort);
+	control.connection.Connect(OperatorAddress, OperatorPort);
+	foreach(string file in ParameterFiles) {
+	    control.LoadParameters(file);
+	}
+    }
+
+    void Start() {
+	awake_finished = true;
+	foreach (Action<BCI2000Remote> action in onIdle) {
+	    action(control);
+	}
+	if (StartModules) {
+	    StartupModules();
+	}
+	control.WaitForSystemState(SystemState.Connected);
+	foreach(Action<BCI2000Remote> action in onConnected) {
+	    action(control);
+	}
+	initialized = true;
+	if (StartWithScene) {
+	    control.Start();
+	}
+    }
+
+    void OnApplicationQuit() {
+	if (StopWithScene) {
+	    control.Stop();
+	}
+	if (ShutdownWithScene) {
+	    control.Quit();
+	}
     }
 
 
